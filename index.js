4138030733d58b96125eb0674cb2bece31d285e8
temple7447 <starukido@gmail.com>
2025-07-01 16:37:18 +0100

    you are

--- index.js
+++ index.js
@@ -102,6 +102,6 @@ app.post('/upload-file', upload.single('file'), async (req, res) => {
 
 // Start server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
